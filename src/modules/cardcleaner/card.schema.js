a3f9e20c7f5cfc62b4cffcbc32f59a3b72539b41
ryanluc7reis <ryan.dev18@outlook.com>
2024-04-09 03:56:05 -0300

    Criado schema  de card cleaner

---
+++ src/modules/cardcleaner/card.schema.js
@@ -0,0 +1,11 @@
+import Joi from 'joi'
+
+import joiObjectid from 'joi-objectid'
+Joi.objectId = joiObjectid(Joi)
+
+export const createCardSchema = Joi.object({
+    name: Joi.string().required().max(25),
+    price: Joi.string().required(),
+    experience: Joi.string().required().max(256),
+    amountCleaning: Joi.number().required(),
+  })
