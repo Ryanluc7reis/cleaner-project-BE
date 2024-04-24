9544898b91a86d5f5ce4d5910cfe76cc5b33b203
ryanluc7reis <ryan.dev18@outlook.com>
2024-04-24 16:29:41 -0300

    Criado funcao getNotifications + deleteNotification

--- src/modules/notifications/notification.service.js
+++ src/modules/notifications/notification.service.js
@@ -3,7 +3,33 @@ import Notification from './notification.model'
 export const createNotification = async (body) => {
   return await Notification.create({   
     for: body.for,
-    notificationType: body.notificationType
+    notificationType: body.notificationType,
+    createdDate: new Date()
   
+  })
+}
+export const getNotifications = async (fullName, limit = 10) => {
+  try {
+    const user = await Notification.findOne(
+      {
+        for: fullName 
+      }
+    )
+    if ( fullName && fullName !== user.for) {
+      throw new Error('user not found')
+    } 
+    const notifications = await Notification.find({ for: fullName }).sort({ createdDate: -1 }).limit(limit)
+       if (!notifications ) {
+      throw new Error('nenhuma notification encontrada');
+    }
+     return notifications
+    
+  } catch (err) {
+    throw err
+  }
+}
+export const deleteNotification = async (id) => {
+  return await Notification.findOneAndDelete({
+    _id: id
   })
 }
