f91d0446bef5a7edae5506592a6791b22d33ec31
ryanluc7reis <ryan.dev18@outlook.com>
2024-05-01 17:53:53 -0300

    Criado rota createRating

---
+++ src/controllers/rating/index.js
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { celebrate, Segments } from 'celebrate';
+
+import { createRating  } from '../../modules/ratingcleaner/rating.service';
+import { createRatingSchema} from '../../modules/ratingcleaner/rating.schema';
+import { verifyToken } from '../../../utils/auth';
+
+
+const router = Router();
+
+router.post('/createRating', verifyToken, celebrate({ [Segments.BODY]: createRatingSchema }), async (req, res) => {
+    try {     
+        const newRating = await createRating(req.body, req.fullName)
+        if (newRating) return res.status(201).send(newRating)
+
+        return res.status(400).send('rating not created')
+      } catch (err) {
+        res.status(500).send(err.message)
+     
+      }    
+});
+
+router.use((err, req, res, next) => {
+    if (err.joi) {
+      return res.status(400).json({
+        error: 'Erro de validação',
+        details: err.joi.details.map(detail => detail.message)
+      });
+    }
+    console.error(err);
+    res.status(500).send('Erro interno do servidor');
+  });
+  export default router
