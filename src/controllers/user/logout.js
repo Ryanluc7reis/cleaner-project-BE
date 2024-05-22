c5b32bd4b9d5f8bbef1916968fc0af0a5fe6cd61
ryanluc7reis <ryan.dev18@outlook.com>
2024-05-22 02:35:50 -0300

    Resolvendo bug de logout

--- src/controllers/user/logout.js
+++ src/controllers/user/logout.js
@@ -1,21 +1,19 @@
 import { Router } from 'express';
 import dotenv from 'dotenv';
+import InvalidToken from '../../modules/invalidTokens/invalidToken.model';
 dotenv.config();
 
 const logout = Router();
 const AUTH_NAME = process.env.SESSION_TOKEN_NAME;
-export const invalidTokens = new Set();
 
-logout.post('/logout', (req, res) => {
+logout.post('/logout', async (req, res) => {
   try {
     const token = req.headers[AUTH_NAME];
     if (!token) return res.status(401).send('Token não fornecido.');
 
-    if (invalidTokens.has(token)) {
-      return res.status(401).send('Token já invalidado.');
-    }
-    
-    invalidTokens.add(token);
+    const invalidToken = new InvalidToken({ token });
+    await invalidToken.save();
+
     res.status(200).json({ message: 'Logout realizado com sucesso' });
   } catch (err) {
     console.error('Erro durante o logout:', err);
--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 import { sign, verify } from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { invalidTokens } from '../src/controllers/user/logout'
+import InvalidToken from '../src/modules/invalidTokens/invalidToken.model';
 dotenv.config(); 
 
 const AUTH_SECRET = process.env.SESSION_PASSWORD;
@@ -11,26 +11,32 @@ export const generateAccessToken = (data) => {
   return token;
 };
 
-export const verifyToken = (req, res, next) => {
+export const verifyToken = async (req, res, next) => {
   const token = req.headers[AUTH_NAME];
   if (!token) {
     return res.status(401).json({ message: 'Token não fornecido' });
   }
 
-  if (invalidTokens.has(token)) {
-    return res.status(401).json({ message: 'Token inválido' });
-  }
-
-  verify(token, AUTH_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: 'Falha ao autenticar o token' });
-    } else {   
-      req.user = decoded.user  
-      req.userId = decoded.userId
-      req.email = decoded.email
-      req.fullName = decoded.fullName
-   
-      next(); 
+  try {
+    const verifyInvalidToken = await InvalidToken.findOne({ token });
+    if (verifyInvalidToken) {
+      return res.status(401).json({ message: 'Token inválido' });
     }
-  });
+
+    verify(token, AUTH_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: 'Falha ao autenticar o token' });
+      } else {
+        req.user = decoded.user;
+        req.userId = decoded.userId;
+        req.email = decoded.email;
+        req.fullName = decoded.fullName;
+
+        next(); 
+      }
+    });
+  } catch (err) {
+    console.error('Erro ao verificar token:', err);
+    return res.status(500).json({ message: 'Erro no servidor' });
+  }
 };
